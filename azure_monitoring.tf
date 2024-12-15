941eed1a769fd23414bd783e74aa19298be4bd1b
Jonah Frany <jonahfrany129@gmail.com>
2024-12-15 23:17:37 +0000

    Mirror from Azure DevOps
      Source Commit: 4b72d8453c4ea5f8f8455b2c6c4078ca30458c11
      Author: Jonah Frany
      Original Message: Mirror from Azure DevOps
    
      Source Commit: 3559f4e8d2524ee1dc9f2bfd66c17ebb9bc13466
      Author: Jonah Frany
      Original Message: Mirror from Azure DevOps
    
      Source Commit: 998f0c400fe817590afdc3a1328052d3c85e4b36
      Author: Jonah Frany
      Original Message: Branch: main
    
      Direct commit to main
    
      Build: terraform-deploy#4.4.17.58
      Pipeline: XMPro Development/terraform-deploy
      Triggered by: Jamester Go
      [skip ci]
    
      Direct commit to main
    
      Build: terraform-deploy#4.4.17.85
      Pipeline: XMPro Development/terraform-deploy
      Triggered by: Jamester Go
      [skip ci]
    
      Direct commit to main
    
      Build: terraform-deploy#4.4.17.92
      Pipeline: XMPro Development/terraform-deploy
      Triggered by: Microsoft.VisualStudio.Services.TFS
      [skip ci]

--- azure_monitoring.tf
+++ azure_monitoring.tf
@@ -1,14 +1,15 @@
-resource "azurerm_application_insights" "appinsights" {
-  name                = "appi-${var.prefix}-${var.environment}-${var.location}"
-  location            = azurerm_resource_group.xmprodocker.location
-  resource_group_name = azurerm_resource_group.xmprodocker.name
-  application_type    = "web"
-}
-
 resource "azurerm_log_analytics_workspace" "logs" {
   name                = "${var.prefix}-logs-workspace"
   location            = azurerm_resource_group.xmprodocker.location
   resource_group_name = azurerm_resource_group.xmprodocker.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "appi-${var.prefix}-${var.environment}-${var.location}"
+  location            = azurerm_resource_group.xmprodocker.location
+  resource_group_name = azurerm_resource_group.xmprodocker.name
+  application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.logs.id
 }
